8c0001001677eeb096ccecc184c33d88a0582611
olu Awoga <seunsmooth@yahoo.com>
2023-11-15 23:28:22 +0000

    fixing error on ami creation

--- packer/scripts/packer_install_scripts.sh
+++ packer/scripts/packer_install_scripts.sh
@@ -6,7 +6,7 @@ export PACKER_LOG_PATH="packerlog.txt"
 echo $PACKER_LOG 
 echo $PACKER_LOG_PATH
 AMI_IDa=`packer build -machine-readable ../techbleat_bg/packer/packer.json | tee build.log` 
-AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
+AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2` 
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > ../techbleat_bg/terraform/amivar.tf
 
 sudo apt-get install -y docker.io
